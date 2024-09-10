336cf7ad6093049a5597dbfcafa2bfc89e0e3d53
mayura-andrew <mayuraalahakoon@gmail.com>
2024-09-10 13:25:03 +0530

    Implement token based user account activation

---
+++ migrations/000005_create_tokens_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS tokens (
+    hash bytea PRIMARY KEY,
+    user_id UUID NOT NULL REFERENCES users ON DELETE CASCADE,
+    expiry TIMESTAMP(0) WITH TIME ZONE NOT NULL,
+    scope text NOT NULL
+);
