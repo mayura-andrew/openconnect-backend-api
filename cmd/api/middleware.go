c408c79e4c3b4da49c09dd525404026cd36f8718
mayura-andrew <mayuraalahakoon@gmail.com>
2024-09-16 15:36:08 +0530

    Refactor error handling and authentication middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -120,3 +120,30 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) requireAuthenticatedUser(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+
+		if user.IsAnonymous(){
+			app.authenticationRequiredResponse(w, r)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+
+func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+
+		if !user.Activated {
+			app.errorResponse(w, r, http.StatusForbidden, "user account not activated")
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+
+	return app.requireAuthenticatedUser(fn)
+}
